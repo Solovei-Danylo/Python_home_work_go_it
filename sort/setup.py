7d44a832bd3149e84be8e42fbe5cf06d7d7ee88e
Solovei-Danylo <trademort@gmail.com>
2022-09-25 21:24:11 +0300

    Mb Final

--- sort/setup.py
+++ sort/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
-setup(name='Python_sort_files_finish',
-      version='0.1',
+setup(name='sort',
+      version='1',
       description='Сортує задану папку- перевіряє внутрішні вкладення/ Форматує утворюючи нові папки і розархивовує архіви, також переіменовує.',
       url='https://github.com/Solovei-Danylo/Python_home_work_go_it',
       author='Solovei-Danylo',
       author_email='',
       license='License',
-      packages=find_namespace_packages(),
-      entry_points={'console_scripts': ['python_sort = Python_sort_files_finish.Sort_files:main']})
+      packages=['sort'],
+      entry_points={'console_scripts': ['clean_folder = sort.sort:main']})
